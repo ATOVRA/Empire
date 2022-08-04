1c7dff4fe0b1f0c44fba90e119c93cfe3f326948
ATOVRA <bashatovibragim2000@gmail.com>
2022-08-04 17:26:43 +0500

    My 2.Project

---
+++ Web Site 2/script.js
@@ -0,0 +1,17 @@
+// window.onscroll = function() {scrollFunction()};
+
+// function scrollFunction(){
+//     console.log( document.documentElement.scrollTop);
+//     if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80){
+//         document.querySelector(".first-container").style.background = "#fff";
+//         document.querySelector(".first-logo").style.color = "#000";
+//         document.querySelectorAll(".menu-list").style.color = "#000";
+//         document.querySelectorAll(".number-div").style.border = " 2px solid #000";
+
+        
+//     }else{
+//         document.querySelector(".first-container").style.background = "none";
+//         document.querySelector(".first-logo").style.color = "white";
+//         document.querySelectorAll(".menu-list").style.color = "#f9f9f9";
+//     }
+// }
